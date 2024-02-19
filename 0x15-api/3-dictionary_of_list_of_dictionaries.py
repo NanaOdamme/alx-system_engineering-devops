7005c1d5008687f7af5f3eceebed5ec0b3468e85
NanaOdamme <eronastyles@gmail.com>
2024-02-19 14:29:08 +0000

    updated'

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,87 +1,18 @@
 #!/usr/bin/python3
-'''A script that gathers employee name completed
-tasks and total number of tasks from an API,
-and exports the data in JSON format.
-'''
-
-import requests
+"""Exports to-do list information of all employees to JSON format."""
 import json
-import sys
+import requests
 
-REST_API = "https://jsonplaceholder.typicode.com"
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
 
-
-def fetch_all_employees_data():
-    """
-    Fetch data for all employees from the API.
-
-    Returns:
-        dict: Data for all employees.
-    """
-    users_url = f"{REST_API}/users"
-    response = requests.get(users_url)
-
-    try:
-        response.raise_for_status()
-        employees = response.json()
-        return {employee['id']: employee['name'] for employee in employees}
-    except requests.exceptions.RequestException as e:
-        print(f"Error: Unable to fetch data for all employees from API - {e}")
-        sys.exit(1)
-
-
-def fetch_tasks_data():
-    """
-    Fetch tasks data from the API.
-
-    Returns:
-        list: List of tasks.
-    """
-    tasks_url = f"{REST_API}/todos"
-    response = requests.get(tasks_url)
-
-    try:
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error: Unable to fetch tasks data from API - {e}")
-        sys.exit(1)
-
-
-def export_to_json(employees_data, tasks):
-    """
-    Export data to JSON format.
-
-    Args:
-        employees_data (dict): Data for all employees.
-        tasks (list): List of tasks.
-    """
-    json_filename = "todo_all_employees.json"
-    json_data = {}
-
-    for task in tasks:
-        user_id = task.get("userId")
-        username = employees_data.get(user_id, "Unknown")
-
-        if user_id not in json_data:
-            json_data[user_id] = []
-
-        json_data[user_id].append({
-            "username": username,
-            "task": task.get("title"),
-            "completed": task.get("completed"),
-        })
-
-    with open(json_filename, mode="w") as json_file:
-        json.dump(json_data, json_file, indent=2)
-
-    print(f"Data exported to {json_filename}")
-
-
-if __name__ == '__main__':
-    employees_data = fetch_all_employees_data()
-    tasks_data = fetch_tasks_data()
-
-    print("Exporting data for all employees...")
-
-    export_to_json(employees_data, tasks_data)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
