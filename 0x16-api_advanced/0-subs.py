da8d1101a8b44d35bcafd3c80cad227807ec8324
NanaOdamme <eronastyles@gmail.com>
2024-03-11 08:24:59 +0000

    updated 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
 """
-number of subscribers for a given subreddit
+Number of subscribers for a given subreddit
 """
 
-from requests import get
-
+import requests
 
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and returns the number of subscribers
+    Function that queries the Reddit API and returns the number of subscribers
     (not active users, total subscribers) for a given subreddit.
     """
 
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
+    user_agent = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.3'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    
     try:
-        return results.get('data').get('subscribers')
+        response = requests.get(url, headers=user_agent)
+        response.raise_for_status()  # Check for HTTP errors
 
-    except Exception:
+        data = response.json()
+        return data['data']['subscribers']
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
         return 0
